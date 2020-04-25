c8afdec71d7310705ebbb7b768436ac48581dba9
jauntydl <57422888+jauntydl@users.noreply.github.com>
2020-04-25 12:38:13 -0400

    Final Submission

--- Mission_to_Mars/app/Mission_to_Mars.py
+++ Mission_to_Mars/app/Mission_to_Mars.py
@@ -1,64 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import datetime as dt
 import time
 
-
 def scrape_all():
 
     # Initiate headless driver for deployment
-    browser = Browser("chrome", executable_path="chromedriver", headless=True)
-    news_title, news_paragraph = mars_news(browser)
+    news_title, news_paragraph = mars_news()
+
 
     # Run all scraping functions and store in dictionary.
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "hemispheres": hemispheres(browser),
-        "weather": twitter_weather(browser),
+        "featured_image": featured_image(),
+        "hemispheres": hemispheres(),
+        "weather": twitter_weather(),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now()
     }
 
     # Stop webdriver and return data
-    browser.quit()
     return data
 
+def mars_news():
 
-def mars_news(browser):
+    executable_path = {'executable_path': 'chromedriver'}
+    browser = Browser('chrome', **executable_path)
 
     url = 'https://mars.nasa.gov/news/'
+
     browser.visit(url)
     browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
     html = browser.html
     soup = bs(html, 'html.parser')
+    browser.quit()
 
     slide = soup.find('li', class_='slide')
-    news_title = slide.find("div", class_='content_title').text
-    news_p = slide.find("div", class_='article_teaser_body').text
 
-    return news_title, news_p
+    L_headline = slide.find("div", class_ = 'content_title').text
+    L_article = slide.find("div", class_ = 'article_teaser_body').text
+
+    
+
+    return L_headline, L_article
 
 
-def featured_image(browser):
+def featured_image():
+
+    executable_path = {'executable_path': 'chromedriver'}
+    browser = Browser('chrome', **executable_path)
 
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 
     browser.visit(url)
     html = browser.html
     soup = bs(html, 'html.parser')
+    browser.quit()
 
-    s = soup.find('article', class_='carousel_item')['style']
+    s = soup.find('article', class_ = 'carousel_item')['style']
+    featured_img_url = 'https://www.jpl.nasa.gov'+s[(s.find("('") + len("('")) : s.find("')")]
 
-    img_url = 'https://www.jpl.nasa.gov' + s[(s.find("('") + len("('")): s.find("')")]
-
-    return img_url
+    return featured_img_url
 
 
-def hemispheres(browser):
+def hemispheres():
+
+    executable_path = {'executable_path': 'chromedriver'}
+    browser = Browser('chrome', **executable_path)
 
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
@@ -66,7 +83,7 @@ def hemispheres(browser):
 
     html = browser.html
     soup = bs(html, 'html.parser')
-
+   
     links = soup.find_all('a', class_ ='itemLink product-item')
 
     link_list = []
@@ -96,29 +113,29 @@ def hemispheres(browser):
         Dict = {"title": title, "img_url": link.get('href')}
         hemisphere_image_urls.append(Dict)
 
+    browser.quit()
+
     return hemisphere_image_urls
 
 
-def twitter_weather(browser):
+def twitter_weather():
+    executable_path = {'executable_path': 'chromedriver'}
+    browser = Browser('chrome', **executable_path)
 
     url = 'https://twitter.com/marswxreport?lang=en'
     browser.visit(url)
-    browser.is_element_present_by_css("div.aria-label", wait_time=5)
+    browser.is_element_present_by_css("div.aria-label", wait_time=3)
 
     html = browser.html
     soup = bs(html, 'html.parser')
 
     main = soup.find('main')
+    browser.quit()
 
     def contains_word(t):
         return t and 'InSight sol' in t
 
-
-    try:
-        mars_weather = main.find('span', text=contains_word).text
-    except AttributeError:
-        return None
-
+    mars_weather = main.find('span',text = contains_word).text
     return mars_weather
 
 
@@ -139,3 +156,10 @@ if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
+
+# In[ ]:
+
+
+
+
--- Mission_to_Mars/app/app.py
+++ Mission_to_Mars/app/app.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import scrape_mars
+import Mission_to_Mars
 
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-# mongo = PyMongo(app)
-
-# Or set inline
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
@@ -21,10 +16,12 @@ def index():
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
-    mars_data = scrape_mars.scrape_all()
-    mars.update({}, mars_data, upsert=True)
-    return render_template("index.html", mars=mars_data)
+    mars_data = Mission_to_Mars.scrape_all()
+    mars.replace_one({}, mars_data, upsert=True)
+    return "Scraping Successful!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
